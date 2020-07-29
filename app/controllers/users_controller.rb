d43ae9b734502dfe2467d826b26d06c94882b66b
kikuishimoto <kikuish@gmail.com>
2020-07-29 16:36:14 -0700

    added views for create. made routes for login. created homepage.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,14 +4,15 @@ class UsersController < ApplicationController
     end 
 
     def create
-        @user = User.create(user_params)
-        if @user.valid?
-            session[:user_id] = user.id
-            redirect_to '/dashboard'
+        @user = User.new(user_params)
+        if @user.save
+            session[:user_id] = @user.id
+            redirect_to trails_path
         else 
             render :new 
         end 
     end
+ 
     private
 
     def user_params
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  root 'welcome#home'
   get '/signup', to: 'users#new'
   post '/signup', to: 'users#create'
+
+  get '/login', to: 'sessions#new'
+  post '/signup', to: 'sessions#create'
 
   resources :comments
   resources :trails
