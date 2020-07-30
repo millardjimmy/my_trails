92f2bbf0914e091740c0ab10b247d3faf1f63cad
kikuishimoto <kikuish@gmail.com>
2020-07-29 17:57:06 -0700

    Added new and create actions

--- app/controllers/trails_controller.rb
+++ app/controllers/trails_controller.rb
@@ -1,6 +1,14 @@
 class TrailsController < ApplicationController
+    before_action :redirect_if_not_signed_in
     def index
-        redirect_if_not_logged_in 
         @trails = Trail.all
     end 
+    def new 
+        @trail = Trail.new
+    end 
+    def create 
+        @trail = current_user.trails.build(trail_params)
+        redirect_to trail_path(@trail)
+    end 
+    
 end
