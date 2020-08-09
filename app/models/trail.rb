af44d1228a0c8a367987a6700ff22620292d61b3
kikuishimoto <kikuish@gmail.com>
2020-08-09 15:49:34 -0700

    added validations for trail

--- app/models/trail.rb
+++ app/models/trail.rb
@@ -4,8 +4,21 @@ class Trail < ApplicationRecord
     belongs_to :user
     accepts_nested_attributes_for :comments, :user
 
+    validates :name, presence: true, uniqueness: true
+    validates :city, presence: true
+    validates :skill_level, presence: true
+    validates :description, presence: true, uniqueness: true
+
+    #scope :search, -> {where(name: params[:search])}
+
+
     def self.most_pop
         #self.joins(:comments).group(:trail_id).order("count("trail_id") DESC")
         self.all.sort_by{|t|t.comments.count}.reverse[0]
     end 
+    def self.search(params)
+        self.where("name LIKE ?", "%#{params[:search]}%")
+    end 
+
+
 end
